6742a0e3d3dc46b9caa831a5febfdf9938ac7315
Volker Berlin <volker.berlin@googlemail.com>
2023-07-05 15:29:28 +0200

    junit dependencies

--- test/com/inet/excel/parser/SheetDimensionTest.java
+++ test/com/inet/excel/parser/SheetDimensionTest.java
@@ -22,7 +22,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import java.util.Arrays;
 import java.util.List;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 public class SheetDimensionTest {
 
